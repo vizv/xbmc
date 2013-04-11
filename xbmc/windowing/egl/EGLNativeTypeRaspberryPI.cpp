b6312df93bf241da1279d9a6a7321a86de87277c
macrule <macrule2001@yahoo.de>
2013-04-11 18:29:03 +0200

    Added private utility function to map a float display aspect, to the respective SDTV_ASPECT_* enum value.

--- xbmc/windowing/egl/EGLNativeTypeRaspberryPI.cpp
+++ xbmc/windowing/egl/EGLNativeTypeRaspberryPI.cpp
@@ -26,6 +26,7 @@
 #include "linux/DllBCM.h"
 #include "utils/StringUtils.h"
 #include "settings/Settings.h"
+#include "math.h"
 
 #ifndef __VIDEOCORE4__
 #define __VIDEOCORE4__
@@ -62,6 +63,7 @@ static float get_display_aspect_ratio(HDMI_ASPECT_T aspect);
 static float get_display_aspect_ratio(SDTV_ASPECT_T aspect);
 static bool ClampToGUIDisplayLimits(int &width, int &height);
 static void SetResolutionString(RESOLUTION_INFO &res);
+static SDTV_ASPECT_T get_sdtv_aspect_from_display_aspect(float display_aspect);
 #endif
 
 
@@ -646,5 +648,24 @@ static void SetResolutionString(RESOLUTION_INFO &res)
     res.dwFlags & D3DPRESENTFLAG_INTERLACED ? "i" : "",
     res.dwFlags & D3DPRESENTFLAG_MODE3DTB   ? " 3DTB" : "",
     res.dwFlags & D3DPRESENTFLAG_MODE3DSBS  ? " 3DSBS" : "");
+}
+
+static SDTV_ASPECT_T get_sdtv_aspect_from_display_aspect(float display_aspect)
+{
+  SDTV_ASPECT_T aspect;
+  const float delta = 1e-3;
+  if(fabs(get_display_aspect_ratio(SDTV_ASPECT_16_9) - display_aspect) < delta)
+  {
+    aspect = SDTV_ASPECT_16_9;
+  }
+  else if(fabs(get_display_aspect_ratio(SDTV_ASPECT_14_9) - display_aspect) < delta)
+  {
+    aspect = SDTV_ASPECT_14_9;
+  }
+  else
+  {
+    aspect = SDTV_ASPECT_4_3;
+  }
+  return aspect;
 }
 #endif
