534e44f43e503986829e9bb04228489613093fd1
popcornmix <popcornmix@gmail.com>
2014-02-04 17:29:37 +0000

    [omxcodec] Add deinterlace support

--- xbmc/cores/VideoRenderers/LinuxRendererGLES.cpp
+++ xbmc/cores/VideoRenderers/LinuxRendererGLES.cpp
@@ -2605,7 +2605,7 @@ bool CLinuxRendererGLES::Supports(EDEINTERLACEMODE mode)
     return true;
 
   if(m_renderMethod & RENDER_OMXEGL)
-    return false;
+    return true;
 
   if(m_renderMethod & RENDER_EGLIMG)
     return false;
