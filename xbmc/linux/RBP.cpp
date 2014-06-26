db4db3027f418e5b79a942f4e6b767cd867502bf
popcornmix <popcornmix@gmail.com>
2014-06-27 00:01:05 +0100

    [rbp] Resume video output on startup

--- xbmc/linux/RBP.cpp
+++ xbmc/linux/RBP.cpp
@@ -145,6 +145,9 @@ bool CRBP::Initialize()
 
   InitializeSettings();
 
+  // in case xbcm was restarted when suspended
+  ResumeVideoOutput();
+
   g_OMXImage.Initialize();
   m_omx_image_init = true;
   return true;
