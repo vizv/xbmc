422e075b99733e5cd6a70f5d12a07416c66a3412
popcornmix <popcornmix@gmail.com>
2014-08-02 17:47:38 +0100

    [VideoReferenceClock] Add OMX support

--- xbmc/video/VideoReferenceClock.h
+++ xbmc/video/VideoReferenceClock.h
@@ -150,6 +150,12 @@ class CVideoReferenceClock : public CThread
 
     int64_t m_LastVBlankTime;  //timestamp of the last vblank, used for calculating how many vblanks happened
                                //not the same as m_VblankTime
+#elif defined(TARGET_RASPBERRY_PI)
+    bool SetupOMX();
+    void RunOMX();
+    void CleanupOMX();
+    int64_t m_LastVBlankTime;  //timestamp of the last vblank, used for calculating how many vblanks happened
+                               //not the same as m_VblankTime
 #endif
 };
 
