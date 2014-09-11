ba1e1025f9b33f079b763b71a781cc2cf9fcaa16
anaconda <anaconda@menakite.eu>
2014-09-11 21:30:43 +0200

    Disable autoscrolling while on screensaver and while opening streams.

--- xbmc/Application.h
+++ xbmc/Application.h
@@ -390,6 +390,8 @@ public:
    */
   void UnregisterActionListener(IActionListener *listener);
 
+  bool ScreenSaverDisablesAutoScrolling();
+
 protected:
   virtual bool OnSettingsSaving() const override;
 
