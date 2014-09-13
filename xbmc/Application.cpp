bee68303d2862fc9fff1974da3d7e9edab743308
anaconda <anaconda@menakite.eu>
2014-09-13 19:49:01 +0200

    SQUASH: only if dim or black

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -5814,3 +5814,10 @@ void CApplication::CloseNetworkShares()
   CSFTPSessionManager::DisconnectAllSessions();
 #endif
 }
+
+std::string CApplication::GetScreenSaverId()
+{
+  if (m_screenSaver)
+    return m_screenSaver->ID();
+  return "";
+}
