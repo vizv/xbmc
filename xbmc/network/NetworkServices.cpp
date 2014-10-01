5ed8499dadf44684a8203d97abbb094dda36fba2
popcornmix <popcornmix@gmail.com>
2014-10-01 19:23:15 +0100

    [airplay] Try DBMandrake's simple iOS8 scheme

--- xbmc/network/NetworkServices.cpp
+++ xbmc/network/NetworkServices.cpp
@@ -530,7 +530,7 @@ bool CNetworkServices::StartAirPlayServer()
   std::vector<std::pair<std::string, std::string> > txt;
   CNetworkInterface* iface = g_application.getNetwork().GetFirstConnectedInterface();
   txt.push_back(make_pair("deviceid", iface != NULL ? iface->GetMacAddress() : "FF:FF:FF:FF:FF:F2"));
-  txt.push_back(make_pair("features", "0x77"));
+  txt.push_back(make_pair("features", "0xF7"));
   txt.push_back(make_pair("model", "Xbmc,1"));
   txt.push_back(make_pair("srcvers", AIRPLAY_SERVER_VERSION_STR));
   CZeroconf::GetInstance()->PublishService("servers.airplay", "_airplay._tcp", g_infoManager.GetLabel(SYSTEM_FRIENDLY_NAME), g_advancedSettings.m_airPlayPort, txt);
