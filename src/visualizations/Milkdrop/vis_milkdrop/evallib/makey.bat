6bd4379cba4f90cfd8f84183e08120f90c36d031
Fneufneu <fneufneu@xbmc.org>
2014-10-10 15:09:51 +0200

    rename xbmc folder to src

---
+++ src/visualizations/Milkdrop/vis_milkdrop/evallib/makey.bat
@@ -0,0 +1,1 @@
+bison -d cal.y
---
+++ src/visualizations/XBMCProjectM/libprojectM/PresetChooser.cpp
@@ -0,0 +1,13 @@
+//
+// C++ Implementation: PresetChooser
+//
+// Description:
+//
+//
+// Author: Carmelo Piccione <carmelo.piccione@gmail.com>, (C) 2007
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#include "PresetChooser.hpp"
