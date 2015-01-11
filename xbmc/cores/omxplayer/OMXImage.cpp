35466be133fa15bedc7327ccc81b4c12409fd1a9
popcornmix <popcornmix@gmail.com>
2015-01-11 21:48:31 +0000

    [omximage] Increase timeout - this is sometimes hit

--- xbmc/cores/omxplayer/OMXImage.cpp
+++ xbmc/cores/omxplayer/OMXImage.cpp
@@ -1885,7 +1885,7 @@ bool COMXImageReEnc::ReEncode(COMXImageFile &srcFile, unsigned int maxWidth, uns
     }
     if (m_encoded_buffer)
     {
-      omx_err = m_omx_encoder.WaitForOutputDone(1000);
+      omx_err = m_omx_encoder.WaitForOutputDone(2000);
       if (omx_err != OMX_ErrorNone)
       {
         CLog::Log(LOGERROR, "%s::%s %s m_omx_encoder.WaitForOutputDone result(0x%x)\n", CLASSNAME, __func__, srcFile.GetFilename(), omx_err);
