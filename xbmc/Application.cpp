5497e33aa1a8e9cf46da399d5c3a4e3995266c44
popcornmix <popcornmix@gmail.com>
2015-01-24 11:22:50 +0000

    Revert "hack: No longer need FrameMove in gui framerate hack"
    This reverts commit 80fef2e96845a9125a43b011277ab3a2231561de.

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -2219,6 +2219,7 @@ void CApplication::Render()
     {
       g_renderManager.FrameWait(100);
       g_infoManager.UpdateFPS();
+      g_renderManager.FrameMove();
       g_renderManager.FrameFinish();
       return;
     }
