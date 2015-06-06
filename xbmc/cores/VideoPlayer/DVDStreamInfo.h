4bb9fd761b3513f850c9d6e20ea1853980f1732f
popcornmix <popcornmix@gmail.com>
2015-06-06 18:43:57 +0100

    ffmpeg: Automatic switch to software decode for GMC with more than one warp point

--- xbmc/cores/VideoPlayer/DVDStreamInfo.h
+++ xbmc/cores/VideoPlayer/DVDStreamInfo.h
@@ -72,6 +72,7 @@ public:
   int orientation; // orientation of the video in degrees counter clockwise
   int bitsperpixel;
   std::string stereo_mode; // stereoscopic 3d mode
+  int workaround_bugs; // info for decoder
 
   // AUDIO
   int channels;
