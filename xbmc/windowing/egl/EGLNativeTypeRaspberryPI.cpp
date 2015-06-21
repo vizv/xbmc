a9a9bc6689b98db3c49b8629e943f0abfbe96afe
popcornmix <popcornmix@gmail.com>
2015-06-21 19:40:22 +0100

    squash: fix dependency of framepacking on mvc

--- xbmc/windowing/egl/EGLNativeTypeRaspberryPI.cpp
+++ xbmc/windowing/egl/EGLNativeTypeRaspberryPI.cpp
@@ -287,7 +287,7 @@ bool CEGLNativeTypeRaspberryPI::SetNativeResolution(const RESOLUTION_INFO &res)
       /* inform TV of any 3D settings. Note this property just applies to next hdmi mode change, so no need to call for 2D modes */
       HDMI_PROPERTY_PARAM_T property;
       property.property = HDMI_PROPERTY_3D_STRUCTURE;
-      if (CSettings::Get().GetBool("videoplayer.framepacking"))
+      if (CSettings::Get().GetBool("videoplayer.framepacking") && CSettings::Get().GetBool("videoplayer.supportmvc"))
         property.param1 = HDMI_3D_FORMAT_FRAME_PACKING;
       else if (stereo_mode == RENDER_STEREO_MODE_SPLIT_VERTICAL)
         property.param1 = HDMI_3D_FORMAT_SBS_HALF;
