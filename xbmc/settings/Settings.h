99f072f61f7d7a1dd71d652b4960f291406a3f20
Jon Frydensbjerg <jonf@vip.cybercity.dk>
2015-12-04 17:48:20 +0000

    High quality downscaling moved to the slideshow category (rendering category axed)

--- xbmc/settings/Settings.h
+++ xbmc/settings/Settings.h
@@ -257,10 +257,10 @@ public:
   static const std::string SETTING_PICTURES_GENERATETHUMBS;
   static const std::string SETTING_PICTURES_SHOWVIDEOS;
   static const std::string SETTING_PICTURES_DISPLAYRESOLUTION;
-  static const std::string SETTING_PICTURES_HIGHQUALITYDOWNSCALING;
   static const std::string SETTING_SLIDESHOW_STAYTIME;
   static const std::string SETTING_SLIDESHOW_DISPLAYEFFECTS;
   static const std::string SETTING_SLIDESHOW_SHUFFLE;
+  static const std::string SETTING_SLIDESHOW_HIGHQUALITYDOWNSCALING;
   static const std::string SETTING_WEATHER_CURRENTLOCATION;
   static const std::string SETTING_WEATHER_ADDON;
   static const std::string SETTING_WEATHER_ADDONSETTINGS;
