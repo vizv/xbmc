29de19ae293c4be505717d1353662ad6a68d7546
Rainer Hochecker <fernetmenta@online.de>
2015-12-09 19:52:57 +0100

    VideoPlayer: adapt a/v sync to changing streams

--- xbmc/cores/VideoPlayer/VideoPlayerAudio.cpp
+++ xbmc/cores/VideoPlayer/VideoPlayerAudio.cpp
@@ -144,7 +144,6 @@ bool CVideoPlayerAudio::OpenStream(CDVDStreamInfo &hints)
   {
     OpenStream(hints, codec);
     m_messageQueue.Init();
-    m_syncState = IDVDStreamPlayer::SYNC_STARTING;
     CLog::Log(LOGNOTICE, "Creating audio thread");
     Create();
   }
@@ -191,6 +190,7 @@ void CVideoPlayerAudio::OpenStream(CDVDStreamInfo &hints, CDVDAudioCodec* codec)
   m_maxspeedadjust = 5.0;
 
   g_dataCacheCore.SignalAudioInfoChange();
+  m_syncState = IDVDStreamPlayer::SYNC_STARTING;
 }
 
 void CVideoPlayerAudio::CloseStream(bool bWaitForBuffers)
