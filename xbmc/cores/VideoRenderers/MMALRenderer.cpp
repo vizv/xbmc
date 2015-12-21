0b97e79f84f60b82b1c57b743a919a592c2e8c3a
popcornmix <popcornmix@gmail.com>
2015-12-21 18:34:06 +0000

    [mmalrender] Force a SetVideoRect after a reconfigure
    There has been an issue with dvd stills and a hdmi refresh rate change.
    The hdmi mode change loses the currently displayed picture.
    Not an issue for normal video playback as another picture will be along soon.
    Not the case in DVD menus.
    
    SetVideoRect makes the last picture redisplay and so fixes up the menu.

--- xbmc/cores/VideoRenderers/MMALRenderer.cpp
+++ xbmc/cores/VideoRenderers/MMALRenderer.cpp
@@ -286,6 +286,10 @@ bool CMMALRenderer::Configure(unsigned int width, unsigned int height, unsigned 
   m_fps = fps;
   m_iFlags = flags;
 
+  // cause SetVideoRect to trigger - needed after a hdmi mode change
+  m_src_rect.SetRect(0, 0, 0, 0);
+  m_dst_rect.SetRect(0, 0, 0, 0);
+
   CLog::Log(LOGDEBUG, "%s::%s - %dx%d->%dx%d@%.2f flags:%x format:%d ext:%x orient:%d", CLASSNAME, __func__, width, height, d_width, d_height, fps, flags, format, extended_format, orientation);
   if (format != RENDER_FMT_YUV420P && format != RENDER_FMT_BYPASS && format != RENDER_FMT_MMAL)
   {
