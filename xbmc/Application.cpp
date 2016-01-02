0ac38f239b75f5ae7d27cda22118ff901ebb28d5
popcornmix <popcornmix@gmail.com>
2016-01-02 20:19:23 +0000

    [debug] Log when playback start takes too long

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -4211,6 +4211,7 @@ bool CApplication::OnMessage(CGUIMessage& message)
 
   case GUI_MSG_PLAYBACK_STARTED:
     {
+      int64_t Start = CurrentHostCounter();
 #ifdef TARGET_DARWIN_IOS
       CDarwinUtils::SetScheduling(message.GetMessage());
 #endif
@@ -4241,6 +4242,10 @@ bool CApplication::OnMessage(CGUIMessage& message)
       param["player"]["speed"] = 1;
       param["player"]["playerid"] = g_playlistPlayer.GetCurrentPlaylist();
       CAnnouncementManager::GetInstance().Announce(Player, "xbmc", "OnPlay", m_itemCurrentFile, param);
+      float duration = (CurrentHostCounter()-Start) * 1e-9;
+      if (duration > 0.1f)
+        CLog::LogF(LOGWARNING, "Suspiciously long time to handle GUI_MSG_PLAYBACK_STARTED (%.2fs)", duration);
+
       return true;
     }
     break;
