2c75bde594242c640ebe9e40ef3d97bb3d681df1
popcornmix <popcornmix@gmail.com>
2016-01-18 20:16:35 +0000

    Revert "[omxplayer] Add ability to dump out audio/video data for later debugging"
    This reverts commit 5a1f52c8c845fb77150dd585cecb3c4869729b9f.

--- xbmc/settings/AdvancedSettings.cpp
+++ xbmc/settings/AdvancedSettings.cpp
@@ -1434,10 +1434,6 @@ void CAdvancedSettings::SettingOptionsLoggingComponentsFiller(SettingConstPtr se
 #ifdef TARGET_RASPBERRY_PI
   list.push_back(std::make_pair(g_localizeStrings.Get(697), LOGOMXPLAYER));
 #endif
-#ifdef TARGET_RASPBERRY_PI
-  list.push_back(std::make_pair(g_localizeStrings.Get(698), LOGDUMPVIDEO));
-  list.push_back(std::make_pair(g_localizeStrings.Get(699), LOGDUMPAUDIO));
-#endif
 }
 
 void CAdvancedSettings::setExtraLogLevel(const std::vector<CVariant> &components)
