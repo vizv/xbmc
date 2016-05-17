e5a7ed02a658beaba21dfd53a8e08d44ebf0e711
popcornmix <popcornmix@gmail.com>
2016-05-17 19:24:08 +0100

    stereoscopics: Switch to using block_lr for mvc to match makemkv
    See: http://forum.kodi.tv/showthread.php?tid=221407&pid=2339656#pid2339656

--- xbmc/cores/VideoPlayer/DVDDemuxers/DVDDemuxFFmpeg.cpp
+++ xbmc/cores/VideoPlayer/DVDDemuxers/DVDDemuxFFmpeg.cpp
@@ -1403,7 +1403,7 @@ CDemuxStream* CDVDDemuxFFmpeg::AddStream(int streamIdx)
                 CDVDInputStreamBluray *bluRay = static_cast<CDVDInputStreamBluray*>(m_pInput);
                 if (bluRay->HasMVC())
                 {
-                  st->stereo_mode = bluRay->AreEyesFlipped() ? "mvc_rl" : "mvc_lr";
+                  st->stereo_mode = bluRay->AreEyesFlipped() ? "block_rl" : "block_lr";
                   mvcStream = static_cast<CDVDDemuxMVC*>(bluRay->GetDemuxMVC())->GetAVStream();
                 }
               }
