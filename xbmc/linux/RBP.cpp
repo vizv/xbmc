65048a4afb4cdce1d03b9d95aa7e50eb8f97f00c
popcornmix <popcornmix@gmail.com>
2017-01-10 11:11:22 +0000

    Revert "temp: Add lock around mmal and omx calls to avoid stalled audio issue"
    This reverts commit 6b2aaa2bd2f4086133c07f4a9c2619193510d6b6.

--- xbmc/linux/RBP.cpp
+++ xbmc/linux/RBP.cpp
@@ -70,7 +70,6 @@ void CRBP::InitializeSettings()
 
 int CRBP::GenCmd(char *response, int maxlen, const char *string)
 {
-  CSingleLock lock(m_critSection);
   return m_DllBcmHost->vc_gencmd(response, maxlen, string);
 }
 
