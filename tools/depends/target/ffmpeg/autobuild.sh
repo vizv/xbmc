25b978e323f3501152deb30577299911799c587f
popcornmix <popcornmix@gmail.com>
2017-02-17 17:58:13 +0000

    ffmpeg: Update hevc optimisation to use the gpu service

--- tools/depends/target/ffmpeg/autobuild.sh
+++ tools/depends/target/ffmpeg/autobuild.sh
@@ -132,14 +132,14 @@ mkdir -p "ffmpeg-${VERSION}"
 cd "ffmpeg-${VERSION}" || exit 2
 tar --strip-components=1 -xf $MYDIR/${ARCHIVE}
 
-patch -p1 < ../0001-mpeg4video-Signal-unsupported-GMC-with-more-than-one.patch
-patch -p1 < ../hevcdsp_ARM_NEON_optimized_epel_functions.patch
-patch -p1 < ../added_ARM_NEON_optimized_SAO_patches.patch
+#patch -p1 < ../0001-mpeg4video-Signal-unsupported-GMC-with-more-than-one.patch
+#patch -p1 < ../hevcdsp_ARM_NEON_optimized_epel_functions.patch
+#patch -p1 < ../added_ARM_NEON_optimized_SAO_patches.patch
 patch -p1 < ../pfcd_hevc_optimisations.patch
-patch -p1 < ../0001-Squashed-commit-of-the-following.patch
-patch -p1 < ../0001-avcodec-add-h264_mvc-codec-id-and-profiles.patch
-patch -p1 < ../0001-h264_parser-add-support-for-parsing-h264-mvc-NALUs.patch
-patch -p1 < ../h264_parser_fix_parsing_of_mvc_slices_in_some_corner_cases.patch
+#patch -p1 < ../0001-Squashed-commit-of-the-following.patch
+#patch -p1 < ../0001-avcodec-add-h264_mvc-codec-id-and-profiles.patch
+#patch -p1 < ../0001-h264_parser-add-support-for-parsing-h264-mvc-NALUs.patch
+#patch -p1 < ../h264_parser_fix_parsing_of_mvc_slices_in_some_corner_cases.patch
 
 CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS" \
 ./configure --prefix=$FFMPEG_PREFIX \
